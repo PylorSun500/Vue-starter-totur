c609d2f2e17bbf1ea4fc2ba11ce88ad26a133829
PylorSun <pylorsun500@outlook.com>
2026-05-12 14:01:19 +0800

    进行了一个组件继承项目

---
+++ Code/works/src/components/kotsets/fathrer.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>父组件当前的值为: {{ number }}</p >
+    <CustomSubComponent @updateCount="updateEmitCount" />
+</template>
+
+<script setup>
+import CustomSubComponent from './CustomSubComponent.vue'
+import { ref } from 'vue'
+const number = ref(10)
+const updateEmitCount = (value) => {
+    number.value += value
+}
+</script>
---
+++ Code/works/src/components/kotsets/values.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>count 值为: {{ count }}</p >
+    <button @click="add">加 n</button>
+</template>
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['updateCount'])
+const count = ref(1)
+const add = () => {
+    count.value++
+    emit('updateCount', 2)
+}
+</script>
