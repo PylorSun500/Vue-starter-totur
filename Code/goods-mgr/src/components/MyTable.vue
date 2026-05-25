d3aff9e42d6d8eb6c70e79f7252a2ba784a8093b
PylorSun500 <pylorsun500@outlook.com>
2026-05-25 10:09:08 +0800

    商品管理实训

---
+++ Code/goods-mgr/src/components/MyTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <table class="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in goodsList" :key="item.id">
+        <slot name="body" :row="item" :index="index"></slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+const props = defineProps({
+  goodsList: {
+    type: Array,
+    required: true,
+    default: []
+  }
+})
+</script>
